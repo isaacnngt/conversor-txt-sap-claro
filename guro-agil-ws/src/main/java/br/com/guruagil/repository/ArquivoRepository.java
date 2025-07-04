aa11c0f6acdf170ce60a86e92083e09adadcd3c6
isaac.nunes <inunes@iblueconsulting.com.br>
2025-07-03 21:19:23 -0300

    retirada de coisas

--- guro-agil-ws/src/main/java/br/com/guruagil/repository/ArquivoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.guruagil.repository;
-
-import br.com.guruagil.entity.Arquivo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ArquivoRepository extends JpaRepository<Arquivo, Long> {
-}
