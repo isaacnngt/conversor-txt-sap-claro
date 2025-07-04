11f5d6bf52bde31344d98ea9ca0fb2ccedeea7f1
isaac.nunes <inunes@iblueconsulting.com.br>
2025-07-03 21:09:57 -0300

    Initial commit

---
+++ convert_txt_excel/src/main/java/br/com/convert_txt/ConvertTxtExcelApplication.java
@@ -0,0 +1,13 @@
+package br.com.convert_txt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvertTxtExcelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConvertTxtExcelApplication.class, args);
+    }
+
+}
---
+++ convert_txt_excel/src/test/java/br/com/convert_txt/ConvertTxtExcelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.convert_txt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvertTxtExcelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
